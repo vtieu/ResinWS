503cd3e68f519cba2354bb6f2f8dd62aca15f8ef
Vincent Tieu <vtieu@yahoo.com>
2014-03-17 11:09:33 -0700

    Refactoring javascript code in index.html to separate js/EventHander.js

---
+++ DynamicHelloWorld/WebContent/js/EventHandler.js
@@ -0,0 +1,34 @@
+function addEventHandler(oNode, sEvt, fHandler, bCapture)
+{
+	if ( typeof (oNode.attachEvent) != "undefined" )
+		oNode.attachEvent("on" + sEvt, fHandler);
+	else
+		oNode.addEventListener(sEvt, fHandler, bCapture);
+}
+
+function onOkClicked( e) {
+	var helloRequest = { name: $("#name").val() };
+	var helloJson = JSON.stringify(helloRequest);
+	var helloParams = $.param({helloRequest: helloJson});
+    jQuery.ajax({
+	    type: "POST",
+	    url: "hello_test/hello/json",
+	    data: helloParams,
+	    dataType: "json",
+	    success: function(result) {
+	    	$("#hello").text(result.helloResult);
+	    }
+    });
+}
+
+
+function setUpClickHandler( ) {
+	var oNode = $("#okButton")[0];
+	addEventHandler(oNode, "click", onOkClicked, false);
+}
+
+
+addEventHandler(window, "load", setUpClickHandler, false);
+
+
+alert("You click ok.");
